b68a769bcc3b62ca0ad9d3fec7d7276b78da59f2
BrechtWillems <brecht.willems2@student.howest.be>
2018-04-22 14:41:14 +0200

    Added Index action and view

--- Routing2/Controllers/BandLidController.cs
+++ Routing2/Controllers/BandLidController.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Routing2.Controllers
-{
-    public class BandLidController : Controller
-    {
-        public IActionResult Index()
-        {
+namespace Routing2.Controllers {
+    [Route("")]
+    [Route("[controller]")]
+    public class BandLidController : Controller {
+
+        [Route("[action]")]
+        public IActionResult Index() {
+            ViewBag.message = "Nothing to see here.";
             return View();
         }
     }
