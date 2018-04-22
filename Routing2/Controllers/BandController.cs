afadd1c071a2117157eaa726ba10c14092270049
BrechtWillems <brecht.willems2@student.howest.be>
2018-04-22 14:15:55 +0200

    Added Dependecy and BandController

---
+++ Routing2/Controllers/BandController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Routing2.Controllers
+{
+    public class BandController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
