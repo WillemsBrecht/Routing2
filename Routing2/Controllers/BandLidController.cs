798ba0741b530f5627d8169d706f7d729b822428
BrechtWillems <brecht.willems2@student.howest.be>
2018-04-22 14:53:05 +0200

    Changed maak view

--- Routing2/Controllers/BandLidController.cs
+++ Routing2/Controllers/BandLidController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using MyClasses;
 
 namespace Routing2.Controllers {
     [Route("")]
@@ -12,6 +13,14 @@ namespace Routing2.Controllers {
         [Route("[action]")]
         public IActionResult Index() {
             ViewBag.message = "Nothing to see here.";
+            return View();
+        }
+
+        //[Route("[action]/[controller]/{name}/{age:int}/{geslacht:GenderOptions}/{bandname}")]
+        [Route("[action]/[controller]/{name}/{age:int}/{geslacht:GenderOptions}/{bandname}")]
+        public IActionResult Maak(string name, int age, GenderOptions geslacht, string bandname) {
+            ViewBag.Bandmember = new BandMember { Name = name, Age = age, Gender = geslacht };
+            ViewBag.Band = bandname;
             return View();
         }
     }
