13d9f433440124947315b83c1ca4174754c0f30a
BrechtWillems <brecht.willems2@student.howest.be>
2018-04-22 14:17:25 +0200

    Added an action and a view

--- Routing2/Controllers/BandController.cs
+++ Routing2/Controllers/BandController.cs
@@ -4,12 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Routing2.Controllers
-{
-    public class BandController : Controller
-    {
-        public IActionResult Index()
-        {
+namespace Routing2.Controllers {
+    public class BandController : Controller {
+        public IActionResult Index() {
+            ViewBag.message = "Nothing to see here.";
             return View();
         }
     }
