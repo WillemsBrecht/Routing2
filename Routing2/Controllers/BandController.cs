da4b38a806a92f5d44f3c537e047a4d1b201b82b
BrechtWillems <brecht.willems2@student.howest.be>
2018-04-22 14:26:31 +0200

    Added maak action

--- Routing2/Controllers/BandController.cs
+++ Routing2/Controllers/BandController.cs
@@ -3,11 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using MyClasses;
 
 namespace Routing2.Controllers {
+    [Route("[controller]")]
     public class BandController : Controller {
+        [Route("[action]")]
         public IActionResult Index() {
             ViewBag.message = "Nothing to see here.";
+            return View();
+        }
+
+        [Route("[action]/[controller]/{bandname:string}/{year:int}")]
+        [Route("[action][controller]/Naam/{bandname:string}/Jaar/{year:int}]")]
+        public IActionResult Maak(string bandname, int year) {
+            ViewBag.Band = new Band { Name = bandname, Year = year };
             return View();
         }
     }
--- Routing2/Startup.cs
+++ Routing2/Startup.cs
@@ -38,13 +38,7 @@ namespace Routing2
             }
 
             app.UseStaticFiles();
-
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
-            });
+            app.UseMvc();
         }
     }
 }
